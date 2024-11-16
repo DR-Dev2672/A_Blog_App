0536ce0da399955f16e3dbedc29aa4ef2ece4f82
dev <thakurdev2672@gmail.com>
2024-11-17 00:08:10 +0530

    bootstap, ejs added

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express=require("express")
+const path=require("path")
 
 const app=express()
 
 const PORT=8001;
 
+app.set("view engine","ejs")
+app.set("views",path.resolve("./views"))
+
 app.get("/",(req,res)=>{
-    res.send("hii from server")
+    res.render("home");
 })
+
+// app.get("/",(req,res)=>{
+//     res.send("hii from server")
+// })
 
 app.listen(PORT,(req,res)=>{
     console.log(`server is started at PORT:${8001}`);
