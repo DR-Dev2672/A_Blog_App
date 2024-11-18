bf7cc633119b53ab86e04471e5b7033b94dc31f3
dev <thakurdev2672@gmail.com>
2024-11-18 16:21:46 +0530

    new requirements added

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express=require("express")
 const path=require("path")
 const userRoute=require("./routes/userRoute")
 
+const cookieParser=require("cookie-parser")
+
 const mongoose=require("mongoose")
+const { checkForAuthenticationCookie } = require("./middlewares/authentication")
 
 
 
 const app=express()
 
+
 const PORT=8001;
+
+
 
 mongoose.connect("mongodb://localhost:27017/blogify")
 .then((e)=>console.log("MongoDB connected"))
@@ -17,8 +23,14 @@ app.set("view engine","ejs")
 app.set("views",path.resolve("./views"))
 app.use(express.urlencoded({extended:false}))
 
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"))
+
 app.get("/",(req,res)=>{
-    res.render("home");
+    res.render("home",{
+        user:req.user
+    });
+
 })
 app.use("/user",userRoute);
 
